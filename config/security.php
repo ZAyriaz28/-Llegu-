64316816cd1c7f07bbd715365f41fce2a3c7defd
Zayri Wilson <zayriazrielwilsonsanchez@gmail.com>
2026-02-19 23:12:57 -0600

    Añadir función de validación de IP para la red INATEC

---
+++ config/security.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Valida si la IP del cliente pertenece a la red autorizada del INATEC
+ *
+ */
+function esRedInatec() {
+    // Lista de IPs públicas autorizadas (Ejemplos)
+    $ips_autorizadas = [
+        '186.77.XXX.XXX', // IP pública del centro Somoto
+        '190.92.XXX.XXX'  // Otra posible IP de salida
+    ];
+
+    // Obtener la IP real del usuario
+    $ip_cliente = $_SERVER['REMOTE_ADDR'];
+
+    // En algunos servidores con proxy se usa:
+    if (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+        $ip_cliente = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    }
+
+    return in_array($ip_cliente, $ips_autorizadas);
+}
+?>
