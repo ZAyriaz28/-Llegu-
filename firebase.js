3f55b5220b49ba77d0302dea968945e201968fe9
Zayri Wilson <zayriazrielwilsonsanchez@gmail.com>
2026-02-23 19:06:54 -0600

    Add Firebase initialization in firebase.js

---
+++ firebase.js
@@ -0,0 +1,22 @@
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _app = __webpack_require__("./public/node_modules/firebase/app/index.js");
+
+var _app2 = _interopRequireDefault(_app);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var app = _app2.default.initializeApp({
+  apiKey: 'AIzaSyBE7Dx4ojDK1KRyC8ru6YGeJGicLXvTMtA',
+  authDomain: 'englishlearning-inatec.firebaseapp.com',
+  databaseURL: 'https://englishlearning-inatec.firebaseio.com',
+  projectId: 'englishlearning-inatec',
+  storageBucket: 'englishlearning-inatec.appspot.com',
+  messagingSenderId: '897690750682'
+});
+
+exports.default = app;//# sourceURL=[module]
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9wdWJsaWMvcmVhY3Qvc3JjL2ZpcmViYXNlLmpzLmpzIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vL3B1YmxpYy9yZWFjdC9zcmMvZmlyZWJhc2UuanM/M2ZlNyJdLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQgZmlyZWJhc2UgZnJvbSBcImZpcmViYXNlL2FwcFwiO1xyXG5cclxuY29uc3QgYXBwID0gZmlyZWJhc2UuaW5pdGlhbGl6ZUFwcCh7XHJcbiAgYXBpS2V5OiAnQUl6YVN5QkU3RHg0b2pESzFLUnlDOHJ1NllHZUpHaWNMWHZUTXRBJyxcclxuICBhdXRoRG9tYWluOiAnZW5nbGlzaGxlYXJuaW5nLWluYXRlYy5maXJlYmFzZWFwcC5jb20nLFxyXG4gIGRhdGFiYXNlVVJMOiAnaHR0cHM6Ly9lbmdsaXNobGVhcm5pbmctaW5hdGVjLmZpcmViYXNlaW8uY29tJyxcclxuICBwcm9qZWN0SWQ6ICdlbmdsaXNobGVhcm5pbmctaW5hdGVjJyxcclxuICBzdG9yYWdlQnVja2V0OiAnZW5nbGlzaGxlYXJuaW5nLWluYXRlYy5hcHBzcG90LmNvbScsXHJcbiAgbWVzc2FnaW5nU2VuZGVySWQ6ICc4OTc2OTA3NTA2ODInXHJcbn0pO1xyXG5cclxuZXhwb3J0IGRlZmF1bHQgYXBwOyJdLCJtYXBwaW5ncyI6Ijs7Ozs7O0FBQUE7QUFDQTs7Ozs7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQU5BO0FBQ0E7QUFRQSIsInNvdXJjZVJvb3QiOiIifQ==
+//# sourceURL=webpack-internal:///./public/react/src/firebase.js
